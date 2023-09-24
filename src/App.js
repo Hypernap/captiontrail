d8a72258a03e182412098932161693112bc31133
Hypernap <sankalp.dubedy4@gmai.com>
2023-09-24 15:34:52 +0530

    Captions on Screen

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import annyang from 'annyang';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      liveCaption: '',
+    };
+  }
+
   componentDidMount() {
     // Initialize annyang
     if (annyang) {
@@ -10,23 +17,42 @@ class App extends React.Component {
     } else {
       console.log('Speech recognition not supported');
     }
+
+    // Set an interval to capture live captions every 3 seconds
+    this.interval = setInterval(this.captureLiveCaption, 3000);
+  }
+
+  componentWillUnmount() {
+    // Clear the interval when the component unmounts
+    clearInterval(this.interval);
   }
 
   handleSpeech = (phrases) => {
     if (phrases.length > 0) {
       const caption = phrases[0];
-      console.log('Live Caption:', caption);
+      // Remove the console.log statement to prevent logging
+      this.setState({ liveCaption: caption });
     }
   };
 
-  handleClick = () => {
-    // No need for anything here since annyang is capturing speech continuously
+  captureLiveCaption = () => {
+    // Manually capture the live caption
+    const { result } = annyang.getSpeechRecognizer();
+    if (result) {
+      this.setState({ liveCaption: result });
+    }
   };
 
   render() {
+    const { liveCaption } = this.state;
+
     return (
       <div className="App">
         <button onClick={this.handleClick}>Start Captioning</button>
+        <div className="live-caption">
+          <p>Live Caption:</p>
+          <div className="caption-text">{liveCaption}</div>
+        </div>
       </div>
     );
   }
